e96cd1a1be7c8d18d2ab7777902841f5b243882c
cubik228 <mkomp06@gmail.com>
2024-02-04 16:57:41 +0300

    write validates for models

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,8 @@
 class Movie < ApplicationRecord
   has_and_belongs_to_many :actors, join_table: :actor_movies
-  belongs_to :category, optional: true
+  belongs_to :category
+  validates :title, presence: true
+  validates :release_year, presence: true
+  validates :description, presence: true
+  validates :rating, presence: true
 end
--- db/migrate/20240204084329_create_movies.rb
+++ db/migrate/20240204084329_create_movies.rb
@@ -1,11 +1,11 @@
 class CreateMovies < ActiveRecord::Migration[7.1]
   def change
     create_table :movies do |t|
-      t.string :title
-      t.integer :release_year
-      t.text :description
-      t.float :rating
-      t.integer :category_id
+      t.string :title,  null: false
+      t.integer :release_year,  null: false
+      t.text :description,  null: false
+      t.float :rating ,  null: false
+      t.integer :category_id, null: false
       t.timestamps
     end
 
