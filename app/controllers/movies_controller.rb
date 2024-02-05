3b3997d17aa8143d9288eb319f62e7a03d5f81a6
cubik228 <mkomp06@gmail.com>
2024-02-05 17:14:24 +0300

    write crud fro controller movies

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,49 @@
+class MoviesController < ApplicationController
+  before_action :set_movie, only: %i[show update destroy]
+
+  def index
+    @movies = Movie.all
+    render json: @movies
+  end
+
+  def new 
+    @movie = Movie.new
+  end
+
+  def show
+    render json: @movie
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    
+    if @movie.save
+      render json: @movie, status: :created
+    else
+      render json: { errors: @movie.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @movie.update(movie_params)
+      render json: @movie
+    else
+      render json: { errors: @movie.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+  
+  def destroy
+    @movie.destroy
+    head :no_content
+  end
+
+  private
+
+  def set_movie
+    @movie = Movie.find(params[:id])
+  end
+
+  def movie_params
+    params.require(:movie).permit(:title, :release_year, :description, :rating, :category_id)
+  end
+end
